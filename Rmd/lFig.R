cc085dc54736756ba714259020444783a9c39db4
lauriekell <laurie@kell.es>
2019-08-05 14:57:47 +0100

    1st commit

---
+++ Rmd/lFig.R
@@ -0,0 +1,53 @@
+lFig   <-"Time series estimates of catch, recruitment, spawning stock biomass and fishing mortality from the 2018 stock assessment; shown with median with 10th, 33th, 66th and 90th percentiles."
+lFig[2]<-"Stock mass, catch mass, maturity, natural mortality and selection pattern at-age"
+lFig[3]<-"Stock mass, catch mass, maturity, natural mortality and selection pattern at-age"
+lFig[4]<-"Relative stock numbers-at-age, i.e. numbers at an age scaled by mean numbers"
+lFig[5]<-"Relative  catch numbers-at-age, i.e. numbers at an age scaled by mean numbers"
+lFig[6]<-"Cross correlations between SSB and recruitment at age 1, a positive lag of 1 would indicate the prescence of a stock recruitment relationship, while a negative lag indicates that SSB is determined by past recruitment"
+lFig[7]<-"Cross correlations between exploitable biomass and recruitment at age 1, a positive lag of 1 would indicate the prescence of a stock recruitment relationship, while a negative lag indicates that SSB is determined by past recruitment"
+lFig[8]<-"Estimates of SSB and recruitment with fitted segmented regression stock recruitment relationship"
+lFig[9]<-"Estimates of SSB and recruitment with fitted Beverton and Holt stock recruitment relationship."
+lFig[10]<-"Estimates of SSB and recruitment with fitted Beverton and Holt stock recruitment relationship with a fixed steepness of 0.9"
+lFig[11]<-"Recruitment deviates for Beverton and Holt stock recruitment relationship, with regimes estimated by STARS algorithm showing changes in mean and variance."
+lFig[12]<-"Recruitment deviates for segmented regression stock recruitment relationship, with regimes estimated by STARS algorithm showing changes in mean and variance."
+lFig[13]<-"Recruitment deviates for Beverton and Holt stock recruitment relationship with steepness fixed at 0.9, with regimes estimated by STARS algorithm showing changes in mean and variance."
+lFig[14]<-"Autocorrelation in recruitment deviates."
+lFig[15]<-"An example of simulated recruitment deviates with autocorrelation."
+lFig[17]<-"Biological reference points based on the fitted segmented regression stock recruitment relationship."
+lFig[16]<-"Biological reference points based on the Beverton and Holt stock recruitment relationship."
+lFig[18]<-"Biological reference points based on the Beverton and Holt stock recruitment relationship with steepness fixed at 0.9."
+lFig[19]<-"F0.1 reference point calculated with a five year moving window"
+lFig[20]<-"Retrospective estimates of time series of catch, recruitment, spawning stock biomass and fishing mortality from the 2018 stock assessment."
+lFig[21]<-"Assessment error in SSB and F derived from the retrospective runs."
+
+lFig[22]<-"Comparison between historical ICES assessment, a projection at Fmsy and HCR1 without assessment error; shown with median with 10 and 90 percentiles."
+
+lFig[23]<-"HCR with and without assessment error; shown with median with 10 and 90 percentiles."
+
+lFig[24]<-"Comparison between HCR I & II with assessment error; shown with median with 10 and 90 percentiles."
+
+lFig[25]<-"Comparison between HCR I without and with bounds; shown with median with 10 and 90 percentiles."
+lFig[26]<-"Comparison between HCR II without and with bounds; shown with median with 10 and 90 percentiles."
+
+lFig[27]<-"Summary of HCR performance."
+
+lFig[28]<-"HCR I without assessment error"
+lFig[29]<-"HCR I plot of F v SSB"
+lFig[30]<-"HCR II plot of F v SSB"
+lFig[31]<-"HCR I plot of F v SSB for 2012 with marginal densities"
+lFig[32]<-"HCR II plot of F v SSB for 2012 with marginal densities"
+lFig[33]<-"Catch summary, total catch and AAV by iteration over simulated period"
+lFig[34]<-"Mean Interannual Annual Absolute Variation over time series by iteration."
+lFig[35]<-"The percentage by year when the stability mechanism was applied."
+lFig[36]<-"Probability that SSB falls below Bpa"
+lFig[37]<-"Probability that SSB falls below BLim."
+
+lFig[38]<-"Comparison of historical stock trends and an escapement harvesting strategy, i.e. take all biomass>Bpa"
+
+eFig=c("Blue whiting SSB as estimated by the last 18 assessments of the stock (conducted in 1999-2015, plus IBPBLW 2016). Time series include forecasted values for y+1 (except for IBPBLW). Prior to 2006 SSB was not estimated for Jan 1. Dotted line = forecasted SSB values from each assessment (i.e. what advice was based on); Red line = IBPLW_2016 assessment (i.e. current 'best' estimate; ICES, 2016a).",
+       "From Skagen (2012), who evaluated a two tier HCR and found it to be precautionary with roughly these parameters: Trigger B1 = 4 Mt, Trigger B2 = 5 Mt and Upper bound F = 0.12 or TAC of about 500 thousand tonnes.",
+       "Default ICES HCR",
+       "Alternative HCRs evaluated as part of MSE of long term management plans in 2016 (WKBMS 2016).")
+
+lFig=c(eFig,lFig)
+
---
+++ Rmd/runHcr.R
@@ -0,0 +1,131 @@
+library(FLasher)
+library(FLBRP)
+library(FLAssess)
+library(ggplotFL)
+library(reshape)
+library(plyr)
+
+library(magrittr)
+library(rdrop2)
+
+## local dir, change at your convenience
+dirMy="/home/laurence/Desktop/sea++/PELAG/bluewhiting"
+
+## access dropbox with data files
+token<-drop_auth()
+saveRDS(token, "/home/laurence/Desktop/Dropbox/token.RDS")
+
+#### get data objects ####################################
+## Operating Model
+## download OM dataset to the current working directory
+## this has stock, srr and FLBRP objects
+drop_download(path='PELAG/bluewhiting/data/om.RData',local_path="data",overwrite=TRUE)
+## stock recruitment deviates
+drop_download(path='PELAG/bluewhiting/data/srDev.RData',local_path="data",overwrite=TRUE)
+
+load(file.path("data","om.RData"))
+load(file.path("data","srDev.RData"))
+
+## change to log scale 
+srDev=exp(srDev)
+
+#### Code to run MSE #####################################
+source(file.path(dirMy,"R/hcrICES.R"))
+
+## Number of Monte Carlo replicates
+nits  =dim(stock.n(om))[6]
+## make sure all slots have same number of iterations
+object=propagate(iter(om,seq(nits)),nits)
+
+### ICES reference points
+refpts=FLPar(c(
+  blim    =1500000,
+  bpa     =2250000,
+  flim    =0.88,
+  fpa     =0.53,
+  msytrig =2250000,
+  fmsy    =0.32,
+  bmgtlow =1500000,
+  bmgt    =2250000,
+  fmgstlow=0.05,
+  fmgt    =0.32),units="NA")
+
+### Parameters for HCRs ##########################
+#### This is a HCR to project at FMSY
+par0=array(c(ftar=refpts["fmsy"],btrig=refpts["msytrig"],fmin=refpts["fmsy"],blim=refpts["blim"]),
+           dim=c(4),
+           dimnames=list(c("ftar","btrig","fmin","blim")))
+
+#### HCR I:
+par1=array(c(ftar=refpts["fmsy"],btrig=refpts["msytrig"],fmin=FLPar(0.05),blim=refpts["blim"]),
+           dim=c(4),
+           dimnames=list(c("ftar","btrig","fmin","blim")))
+
+#### HCR II:
+par2=array(c(0.20,2250000,
+             0.05,1500000,
+             0.32,5200000,
+             0.20,4000000),
+          dim=c(4,2),
+          dimnames=list(c("ftar","btrig","fmin","blim"),c("lower","upper")))[c(1,3,2,4),1:2]
+
+## Start Simulations ################################################
+#### years for simulations
+start   =2001
+end     =2018
+interval=1
+
+#### Assessment error
+err =rlnorm(nits,FLQuant(0,dimnames=list(year=start:end)),0.3)
+
+## Scenarios with steepness=0.9 ####################################
+
+#### Reference cases
+sims=list("ICES"=list(object,NULL))
+## projection for FMSY
+sims[["fmsy"]]=list(fwd(om,fbar=FLQuant(1,dimnames=list(year=2001:2018))%=%0.32,
+                         sr=eql9,residuals=srDev),
+                     NULL)
+## projection for F=0.6
+sims[["0.6"]]=list(fwd(om,fbar=FLQuant(1,dimnames=list(year=2001:2018))%=%0.6,
+                        sr=eql9,residuals=srDev),
+                    NULL)
+## HCR for FMSY without assessment error
+sims[["sim0.0"]]=hcrICES(object,eql9,srDev,
+                      par0,
+                      start=start,end=end,interval=interval)
+## HCR for FMSY with assessment error
+sims[["sim0"]]=hcrICES(object,eql9,srDev,
+                      par0,
+                      start,end,interval,
+                      err=err,
+                      bndTac=c(0,Inf))
+##### HCRs
+sims[["sim1"]]=hcrICES(object,eql9,srDev,
+                      par1,
+                      start,end,interval,
+                      err=err,
+                      bndTac=c(0,Inf))
+sims[["sim2"]]=hcrICES(object,eql9,srDev,
+                      par2,
+                      start,end,interval,
+                      err=err,
+                      bndTac=c(0,Inf))
+# HCRs with bounds 
+sims[["sim0_bnd"]]=hcrICES(object,eql9,srDev,
+                      par0,
+                      start,end,interval,  
+                      err=err,
+                      bndTac=c(0.8,1.25))
+sims[["sim1_bnd"]]=hcrICES(object,eql9,srDev,
+                      par1,
+                      start,end,interval,
+                      err=err,
+                      bndTac=c(0.80,1.25))
+sims[["sim2_bnd"]]=hcrICES(object,eql9,srDev,
+                      par2,
+                      start,end,interval,
+                      err=err,
+                      bndTac=c(0.80,1.25))
+      
+save(sims,file=file.path(dirMy,"data/sims.RData"),compress="xz")
